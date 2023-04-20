2e63206b4795809a1152a2b937ecce9f550c91d8
Alejandro Matutano <alejandro.matutano@evoluciona.com>
2023-04-20 15:42:07 +0200

    fov 35

--- src/App.js
+++ src/App.js
@@ -1,54 +1,67 @@
-import React, { Suspense, useRef } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls, useHelper } from '@react-three/drei'
-import { Model } from './Model'
-import { useControls } from 'leva'
+import React, { Suspense, useEffect, useRef } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Environment, OrbitControls, useHelper } from "@react-three/drei";
+import { Model } from "./Model";
+import { useControls } from "leva";
 
 export default function Viewer() {
-  const ref = useRef()
-  const pL1 = useRef()
-  const pL2 = useRef()
+  const ref = useRef();
+  const pL1 = useRef();
+  const pL2 = useRef();
+  const canvas = useRef();
 
-  const { envMapIntensity, intensity1PointLight, intensity2PointLight, color1PointLight, color2PointLight, position1PointLight, position2PointLight } = useControls({
+  const {
+    fovCamera,
+    envMapIntensity,
+    intensity1PointLight,
+    intensity2PointLight,
+    color1PointLight,
+    color2PointLight,
+    position1PointLight,
+    position2PointLight,
+  } = useControls({
     intensity1PointLight: {
-      value: 0.5,
+      value: 3.0,
       min: 0,
       max: 5,
-      step: 0.01
+      step: 0.01,
     },
     intensity2PointLight: {
-      value: 0.5,
+      value: 1.5,
       min: 0,
       max: 5,
-      step: 0.01
+      step: 0.01,
     },
     envMapIntensity: {
-      value: 1,
+      value: 3,
       min: 0,
       max: 5,
-      step: 0.01
+      step: 0.01,
     },
-    color1PointLight: '#5780FF',
-    color2PointLight: '#5780FF',
+    color1PointLight: "#ffb557",
+    color2PointLight: "#008cff",
     position1PointLight: {
-      value: [-7.9, 8, -7.56],
-      step: 0.01
+      value: [-9000, 0, 0],
+      step: 0.01,
     },
     position2PointLight: {
-      value: [1.89, 9.2, -8.51],
-      step: 0.01
-    },
-  })
+      value: [9000, 0, 0],
+      step: 0.01,
+    }
+  });
 
   return (
-    <Canvas dpr={[1, 2]}
+    <Canvas
+      dpr={[1, 2]}
+      ref={canvas}
       camera={{
         far: 1000000,
-        fov: 100,
+        fov: 35,
         near: 0.5,
-        position: [-200, 500, 1500] // Esto OK para Garray, pero no para Algarrobico ni Buniel.
+        position: [-200, 500, 1500], // Esto OK para Garray, pero no para Algarrobico ni Buniel.
       }}
-      style={{ background: "#1f1f1f" }}>
+      style={{ background: "#1f1f1f" }}
+    >
       <Suspense fallback={null}>
         <Environment files="/Grad5_2k.hdr" />
         <Model envMapIntensity={envMapIntensity} />
@@ -71,5 +84,5 @@ export default function Viewer() {
       </Suspense>
       <OrbitControls ref={ref} />
     </Canvas>
-  )
+  );
 }
